09d0891e55cb1850a0ce215ad0c1b6b97c547baf
Piotr <piotr.skowronski@cern.ch>
2017-08-01 14:05:19 +0200

    Commit before mergin the master from Riccardo

--- build/matchSqueeze.sh
+++ build/matchSqueeze.sh
@@ -1,13 +1,16 @@
 
+betxa=(0.45 0.40 0.35 0.30 0.25 0.20 0.15 0.10 0.095 0.090  0.085  0.080  0.075 )
+betya=(0.45 0.40 0.40 0.40 0.40 0.40 0.40 0.40 0.38 0.36 0.34  0.32   0.30  )
+
 #betxa=(0.40 0.40 0.40 0.40 0.40 0.38  0.36   0.34   0.32   0.30  )
 #betya=(0.35 0.30 0.20 0.15 0.10 0.095 0.090  0.085  0.080  0.075 )
 
-betxa=(0.40 0.40 0.40 0.40 0.40 0.40 0.38  0.36   0.34   0.32   0.30  )
-betya=(0.25 0.23 0.20 0.18 0.15 0.10 0.095 0.090  0.085  0.080  0.075 )
+#betxa=(0.40 0.40 0.40 0.40 0.40 0.40 0.38  0.36   0.34   0.32   0.30  )
+#betya=(0.25 0.23 0.20 0.18 0.15 0.10 0.095 0.090  0.085  0.080  0.075 )
 
-#echo "slhc/squeeze/opt_400_400_400_400.madx" > lastMatchedOpticFileName.txt
+echo "slhc/squeeze/opt_500_500_500_500.madx" > lastMatchedOpticFileName.txt
 
-echo "opt_400_300_300_400_v9.madx" > lastMatchedOpticFileName.txt
+#echo "opt_400_300_300_400_v9.madx" > lastMatchedOpticFileName.txt
 npoints=${#betxa[@]}
 
 point=0
--- build/mk_plot_squeeze.py
+++ build/mk_plot_squeeze.py
@@ -1,7 +1,10 @@
-from pyoptics import *
-from matplotlib.pyplot import *
-from matplotlib.axes import *
-import scipy
+import pyoptics as pop
+import matplotlib.pyplot as plt
+import matplotlib.axes as ax
+import matplotlib.ticker as ticker
+import scipy as sp
+import numpy as np
+import sys
 #close('all')
 #--- compare new/old squeeze
 def plot_squeeze_diff(ir='2468',opt='round'):
@@ -12,71 +15,309 @@ def plot_squeeze_diff(ir='2468',opt='round'):
     print i,max(dsnew['tarir%sb1'%i]),max(dsnew['tarir%sb2'%i])
 
 
-def plot_squeeze(ir='2468',opt='round'):
-  close('all')
+def plot_squeeze_all(ir='2468',opt='round'):
+#  close('all')
   for i in ir:
     fname='ir%s_%s.tfs'%(i,opt)
     print fname
-    ds=StrTable.open(fname)
+    ds=pop.StrTable.open(fname)
     ds.plot_squeeze() # title='squeeze IR%s'%i
     print i,max(ds['tarir%sb1'%i]),max(ds['tarir%sb2'%i])
-    savefig('ir%s_%s.png'%(i,opt))
+    plt.savefig('ir%s_%s.png'%(i,opt))
 
-#for opt in 'round','flat','flathv':
-opt='flat'
-fig=figure(opt)
-ds=StrTable.open('ir6_%s.tfs'%opt)
-plot(ds.betxtcdsb1,'k',label='betx TCDS.A*.B1')
-plot(ds.betytcdsb1,'r',label='bety TCDS.A*.B1')
-plot(ds.betxtcdsb2,'b',label='betx TCDS.A*.B2')
-plot(ds.betytcdsb2,'m',label='bety TCDS.A*.B2')
-xlabel(r'squeeze step (squeeze -> presqueeze)')
-ylabel(r'$\beta_{x/y,\rm TCDS.A}$ [m]')
-title('%s squeeze'%opt)
-legend(loc='best')
-savefig('ir6_%s_beta_tcsg.png'%(opt))
+###########################################################################
+## PLOT_SQ_VARCONSTR
+## -1 var need to be bigger then constrval
+##  0 var need to be eqaul
+## +1 var need to be smaller then constrval
+## +2 var need to be in range
+def plot_sq(ds,varname,title=None,ylab=None,log=False):
+  var = ds[varname]
+  if title is None:
+     title = varname
+  if ylab is None:
+     ylab = varname
+     
+  plt.close('all') 
+  plt.figure(title)
+  if log:
+    plt.semilogy(var,'k',marker='.',label=title)
+  else:
+    plt.plot(var,'k',marker='.',label=title)
+    
+  y_formatter = ticker.ScalarFormatter(useOffset=False)
+  ax1 = plt.gca()
+  ax1.yaxis.set_major_formatter(y_formatter)
+ 
+  plt.xlabel(r'squeeze step ')
+  plt.ylabel(ylab)
+  plt.title(title)
+#  plt.show()
+  plt.savefig('%s.png'%varname)
 
-close('all')
-plot(ds.betxtcdsb1,'k',marker='.',label='betx TCDS.A*.B1')
-
-xs = np.arange(0.0, len(ds.betxtcdsb1), 1)
-
-fill_between(xs, 150, 160, hatch='/',facecolor='white',edgecolor='red')
-
-xlabel(r'squeeze step (squeeze -> presqueeze)')
-ylabel(r'$\beta_{x,b1,\rm TCDS.A}$ [m]')
-title('betxtcdsb1 %s squeeze '%opt)
-savefig('ir6_%s_betxtcdsb1.png'%(opt))
-
-close('all')
-plot(ds.betytcdsb1,'k',marker='.',label='bety TCDS.A*.B1')
-xlabel(r'squeeze step (squeeze -> presqueeze)')
-ylabel(r'$\beta_{y,b1,\rm TCDS.A}$ [m]')
-title('betytcdsb1 %s squeeze '%opt)
-savefig('ir6_%s_betytcdsb1.png'%(opt))
-
-close('all')
-plot(ds.betxtcdsb2,'k',marker='.',label='betx TCDS.A*.B2')
-xlabel(r'squeeze step (squeeze -> presqueeze)')
-ylabel(r'$\beta_{x,b2,\rm TCDS.A}$ [m]')
-title('betxtcdsb2 %s squeeze '%opt)
-savefig('ir6_%s_betxtcdsb2.png'%(opt))
-
-close('all')
-plot(ds.betytcdsb2,'k',marker='.',label='bety TCDS.A*.B2')
-xlabel(r'squeeze step (squeeze -> presqueeze)')
-ylabel(r'$\beta_{y,b2,\rm TCDS.A}$ [m]')
-title('betytcdsb2 %s squeeze '%opt)
-savefig('ir6_%s_betytcdsb2.png'%(opt))
+###########################################################################
+## PLOT_SQ_VARCONSTR
+## -1 var need to be bigger then constrval
+##  0 var need to be eqaul
+## +1 var need to be smaller then constrval
+## +2 var need to be in range
+def plot_sq_varconstr(var,varname,title,ylab,constrval,constrtype, constrval2=np.NaN ):
+  plt.close('all') 
+  plt.plot(var,'k',marker='.',label=title)
+  xs = np.arange(0.0, len(var), 1)
+  
+  if (constrtype == -1):
+    xmin = constrval*0.9
+    datamin = min(var)
+    if ( datamin < xmin):
+      xmin = 0.9*datamin
+    plt.fill_between(xs, xmin, constrval, hatch='/',facecolor='white',edgecolor='red')
 
 
+  if (constrtype == 2):
+    xmin = constrval   - 50
+    xmax = constrval2  + 50
+
+    datamin = min(var)
+    if ( datamin < xmin):
+      xmin = 0.9*datamin
+
+    datamax = max(var)
+    if ( datamax > xmax):
+      xmax = 1.1*datamax
+    
+    print xmin, constrval
+    plt.fill_between(xs, xmin, constrval, hatch='/',facecolor='white',edgecolor='red')
+    
+    print constrval2, xmax
+    plt.fill_between(xs, constrval2, xmax, hatch='/',facecolor='white',edgecolor='red')
+    
+  plt.xlabel(r'squeeze step ')
+  plt.ylabel(ylab)
+  plt.title(title)
+#  plt.show()
+  plt.savefig('%s.png'%varname)
+  
+###############################################################
+###############################################################
+###############################################################
+
+opt='flat' 
+
+###############################################################
+# betx TCDS.A*.B1
+
+#plot_sq_varconstr(ds.betxtcdsb1,'betxtcdsb1','betx TCDS.A*.B1', r'$\beta_{x,b1,\rm TCDS.A}$ [m]',  160, -1)
+
+#plot_sq_varconstr(ds.betytcdsb1,'betytcdsb1','bety TCDS.A*.B1', r'$\beta_{y,b1,\rm TCDS.A}$ [m]',  200, -1)
+#plot_sq_varconstr(ds.betytcdsb2,'betytcdsb2','bety TCDS.A*.B2', r'$\beta_{y,b2,\rm TCDS.A}$ [m]',  200, -1)
 
 
-#plot_squeeze_diff(opt='round')
-#plot_squeeze_diff(opt='flat')
-#plot_squeeze_diff(opt='flathv')
-#plot_squeeze(opt='round')
+
+ds=pop.StrTable.open('ir1_%s.tfs'%opt)
+
+plot_sq(ds,'muxip1b1')
+plot_sq(ds,'muyip1b1')
+plot_sq(ds,'muxip1b1_l')
+plot_sq(ds,'muyip1b1_l')
+plot_sq(ds,'muxip1b1_r')
+plot_sq(ds,'muyip1b1_r')
+
+plot_sq(ds,'muxip1b2')
+plot_sq(ds,'muyip1b2')
+plot_sq(ds,'muxip1b2_l')
+plot_sq(ds,'muyip1b2_l')
+plot_sq(ds,'muxip1b2_r')
+plot_sq(ds,'muyip1b2_r')
+
+
+ds=pop.StrTable.open('ir5_%s.tfs'%opt)
+
+plot_sq(ds,'muxip5b1')
+plot_sq(ds,'muyip5b1')
+plot_sq(ds,'muxip5b1_l')
+plot_sq(ds,'muyip5b1_l')
+plot_sq(ds,'muxip5b1_r')
+plot_sq(ds,'muyip5b1_r')
+
+plot_sq(ds,'muxip5b2')
+plot_sq(ds,'muyip5b2')
+plot_sq(ds,'muxip5b2_l')
+plot_sq(ds,'muyip5b2_l')
+plot_sq(ds,'muxip5b2_r')
+plot_sq(ds,'muyip5b2_r')
+
+
+ds=pop.StrTable.open('ir2_%s.tfs'%opt)
+
+plot_sq(ds,'muxip2b1')
+plot_sq(ds,'muyip2b1')
+plot_sq(ds,'muxip2b1_l')
+plot_sq(ds,'muyip2b1_l')
+plot_sq(ds,'muxip2b1_r')
+plot_sq(ds,'muyip2b1_r')
+
+plot_sq(ds,'muxip2b2')
+plot_sq(ds,'muyip2b2')
+plot_sq(ds,'muxip2b2_l')
+plot_sq(ds,'muyip2b2_l')
+plot_sq(ds,'muxip2b2_r')
+plot_sq(ds,'muyip2b2_r')
+
+ds=pop.StrTable.open('ir8_%s.tfs'%opt)
+
+plot_sq(ds,'muxip8b1')
+plot_sq(ds,'muyip8b1')
+plot_sq(ds,'muxip8b1_l')
+plot_sq(ds,'muyip8b1_l')
+plot_sq(ds,'muxip8b1_r')
+plot_sq(ds,'muyip8b1_r')
+
+plot_sq(ds,'muxip8b2')
+plot_sq(ds,'muyip8b2')
+plot_sq(ds,'muxip8b2_l')
+plot_sq(ds,'muyip8b2_l')
+plot_sq(ds,'muxip8b2_r')
+plot_sq(ds,'muyip8b2_r')
+
+
+ds=pop.StrTable.open('ir4_%s.tfs'%opt)
+
+plot_sq(ds,'muxip4b1')
+plot_sq(ds,'muyip4b1')
+plot_sq(ds,'muxip4b1_l')
+plot_sq(ds,'muyip4b1_l')
+plot_sq(ds,'muxip4b1_r')
+plot_sq(ds,'muyip4b1_r')
+
+plot_sq(ds,'muxip4b2')
+plot_sq(ds,'muyip4b2')
+plot_sq(ds,'muxip4b2_l')
+plot_sq(ds,'muyip4b2_l')
+plot_sq(ds,'muxip4b2_r')
+plot_sq(ds,'muyip4b2_r')
+
+ds=pop.StrTable.open('ir6_%s.tfs'%opt)
+
+plot_sq(ds,'muxip6b1')
+plot_sq(ds,'muyip6b1')
+plot_sq(ds,'muxip6b1_l')
+plot_sq(ds,'muyip6b1_l')
+plot_sq(ds,'muxip6b1_r')
+plot_sq(ds,'muyip6b1_r')
+
+plot_sq(ds,'muxip6b2')
+plot_sq(ds,'muyip6b2')
+plot_sq(ds,'muxip6b2_l')
+plot_sq(ds,'muyip6b2_l')
+plot_sq(ds,'muxip6b2_r')
+plot_sq(ds,'muyip6b2_r')
+
+
+ds=pop.StrTable.open('ir3_%s.tfs'%opt)
+
+
+plot_sq(ds,'muxip3b1')
+plot_sq(ds,'muyip3b1')
+plot_sq(ds,'muxip3b1_l')
+plot_sq(ds,'muyip3b1_l')
+plot_sq(ds,'muxip3b1_r')
+plot_sq(ds,'muyip3b1_r')
+
+plot_sq(ds,'muxip3b2')
+plot_sq(ds,'muyip3b2')
+plot_sq(ds,'muxip3b2_l')
+plot_sq(ds,'muyip3b2_l')
+plot_sq(ds,'muxip3b2_r')
+plot_sq(ds,'muyip3b2_r')
+
+ds=pop.StrTable.open('ir7_%s.tfs'%opt)
+
+plot_sq(ds,'muxip7b1')
+plot_sq(ds,'muyip7b1')
+plot_sq(ds,'muxip7b1_l')
+plot_sq(ds,'muyip7b1_l')
+plot_sq(ds,'muxip7b1_r')
+plot_sq(ds,'muyip7b1_r')
+
+plot_sq(ds,'muxip7b2')
+plot_sq(ds,'muyip7b2')
+plot_sq(ds,'muxip7b2_l')
+plot_sq(ds,'muyip7b2_l')
+plot_sq(ds,'muxip7b2_r')
+plot_sq(ds,'muyip7b2_r')
+
+
+####################################################
+
+sys.exit()
+
+ds=pop.StrTable.open('rng_%s.tfs'%opt)
+
+plot_sq(ds,'dtct1b1')
+plot_sq(ds,'dtct5b1')
+plot_sq(ds,'dtct1b2')
+plot_sq(ds,'dtct5b2')
+
+plot_sq(ds,'dtct1b1a')
+plot_sq(ds,'dtct5b1a')
+plot_sq(ds,'dtct1b2a')
+plot_sq(ds,'dtct5b2a')
+
+
+plot_sq_varconstr(ds['ddtct1b1'],'ddtct1b1','mux B1 TCTXH.4L1 <-> MKD.O5L6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+plot_sq_varconstr(ds['ddtct5b1'],'ddtct5b1','mux B1 TCTXH.4L5 <-> MKD.O5L6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+plot_sq_varconstr(ds['ddtct1b2'],'ddtct1b2','mux B2 TCTXH.4R1 <-> MKD.O5R6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+plot_sq_varconstr(ds['ddtct5b2'],'ddtct5b2','mux B2 TCTXH.4R5 <-> MKD.O5R6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+
+plot_sq_varconstr(ds['ddtct1b1a'],'ddtct1b1a','mux B1 TCTXH.4L1 <-> MKD.A5L6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+plot_sq_varconstr(ds['ddtct5b1a'],'ddtct5b1a','mux B1 TCTXH.4L5 <-> MKD.A5L6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+plot_sq_varconstr(ds['ddtct1b2a'],'ddtct1b2a','mux B2 TCTXH.4R1 <-> MKD.A5R6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+plot_sq_varconstr(ds['ddtct5b2a'],'ddtct5b2a','mux B2 TCTXH.4R5 <-> MKD.A5R6', r'$mu_{x}$ [deg]',-50,2,constrval2=50)
+
+
+sys.exit()
+
+ds=pop.StrTable.open('ir6_%s.tfs'%opt)
+scale=23348.89927
+qtlimit2 = 160.0/scale
+plot_sq(ds,'kq4.l6.b1',title='k1 of KQ4.L6.B1', ylab=r'$\k1_{KQ4.L6.B1}$ ')
+plot_sq(ds,'kq4.r6.b1',title='k1 of KQ4.R6.B1', ylab=r'$\k1_{KQ4.R6.B1}$ ')
+plot_sq(ds,'kq4.l6.b2',title='k1 of KQ4.L6.B2', ylab=r'$\k1_{KQ4.L6.B2}$ ')
+plot_sq(ds,'kq4.r6.b2',title='k1 of KQ4.R6.B2', ylab=r'$\k1_{KQ4.R6.B2}$ ')
+
+
+
+
 
 sys.exit();
-plot_squeeze(opt=opt)
-#plot_squeeze(opt='flathv')
+
+plot_sq(ds,'tarir1b1',log=True)
+plot_sq(ds,'tarir1b2',log=True)
+plot_sq(ds,'tarir2b1',log=True)
+plot_sq(ds,'tarir2b2',log=True)
+plot_sq(ds,'tarir3b1',log=True)
+plot_sq(ds,'tarir3b2',log=True)
+plot_sq(ds,'tarir4b1',log=True)
+plot_sq(ds,'tarir4b2',log=True)
+plot_sq(ds,'tarir5b1',log=True)
+plot_sq(ds,'tarir5b2',log=True)
+plot_sq(ds,'tarir6b1',log=True)
+plot_sq(ds,'tarir6b2',log=True)
+plot_sq(ds,'tarir7b1',log=True)
+plot_sq(ds,'tarir7b2',log=True)
+plot_sq(ds,'tarir8b1',log=True)
+plot_sq(ds,'tarir8b2',log=True)
+
+sys.exit()
+
+ds=pop.StrTable.open('rng_%s.tfs'%opt)
+
+plot_sq(ds,'refqxb1')
+plot_sq(ds,'refqyb1')
+plot_sq(ds,'refqxb2')
+plot_sq(ds,'refqyb2')
+
+
+#plot_squeeze_all(opt=opt)
+
